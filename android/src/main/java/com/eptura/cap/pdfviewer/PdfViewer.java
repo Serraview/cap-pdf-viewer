8aeef7947dc9eefd7c30eebeb51a178ebadce50f
Jeff Martin <jeff_martin@archibus.com>
2022-11-05 12:34:36 -0400

    Initial commit

---
+++ android/src/main/java/com/eptura/cap/pdfviewer/PdfViewer.java
@@ -0,0 +1,11 @@
+package com.eptura.cap.pdfviewer;
+
+import android.util.Log;
+
+public class PdfViewer {
+
+    public String echo(String value) {
+        Log.i("Echo", value);
+        return value;
+    }
+}
